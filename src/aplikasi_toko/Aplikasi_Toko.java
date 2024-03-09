690e4c04a4edbacc3ebb8b4390d01ba5667a1de4
Mohammad Kharis M <khariskingpro@gmail.com>
2024-03-09 08:46:08 +0700

    first commit

---
+++ src/aplikasi_toko/Aplikasi_Toko.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplikasi_toko;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Aplikasi_Toko {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Login l = new Login();
+        l.setVisible(true);
+    }
+    
+}
